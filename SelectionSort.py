65f1bc9d7a6dd2f6ff10c3bd5d9779c0c5daf3a3
anikaaggarwal <anika.aggarwal1@gmail.com>
2024-02-14 14:30:51 -0500

    Create SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,15 @@
+def selectionSort(arr, size):
+
+    for i in range(size):
+        key = i
+        for j in range(i + 1, size):
+            if arr[j] < arr[key]:
+                key = j
+
+        (arr[i], arr[key]) = (arr[key], arr[i])
+
+arr = [5, 75, 4, -145, -10, 22, -202]
+size = len(arr)
+selectionSort(arr,size)
+print('The array after sorting in Ascending Order by selection sort is:')
+print(arr)
