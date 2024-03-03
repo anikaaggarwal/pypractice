6b0a6e749c14730e74e6e561a86d6bff4ca50189
anikaaggarwal <anika.aggarwal1@gmail.com>
2024-03-03 16:03:16 -0500

    Update bucketSort.py

--- bucketSort.py
+++ bucketSort.py
@@ -11,3 +11,18 @@ def bucketSort(array):
         bucket[index_b].append(j)
 
     #sort the elements of each bucket
+    for i in range(len(array)):
+           bucket[i] = sorted(bucket[i])
+
+    # Get the sorted elements
+    k = 0
+    for i in range(len(array)):
+        for j in range(len(bucket[i])):
+            array[k] = bucket[i][j]
+            k += 1
+    return array
+
+array = [.42, .32, .33, .52, .37, .48, .51]
+secondArray = [24, 87, 1, 44, 23, 311, 67, 99, 134]
+print('Sorted Array is')
+print(bucketSort(array))
