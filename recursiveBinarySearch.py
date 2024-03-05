ece142d2ad3165c9031f7a5bcdbe27e3463f09a8
anikaaggarwal <anika.aggarwal1@gmail.com>
2024-03-05 12:56:04 -0500

    Update recursiveBinarySearch.py

--- recursiveBinarySearch.py
+++ recursiveBinarySearch.py
@@ -7,18 +7,19 @@ def binarySearch(array, l, r, element):
         #checking if element is at the middle element
         if array[mid] == element:
             return mid
-        elif array[mid] < element:
-            return binarySearch(array, l, mid + 1, element)
+        elif array[mid] > element:
+            return binarySearch(array, l, mid-1, element)
         else:
-            return binarySearch(array, l, mid - 1, element)
+            return binarySearch(array, mid+1, r, element)
     else:
         return -1
 
-array = [1, 3, 4, 10, 40]
+array = [2, 3, 4, 10, 12]
 element = 10
 
 result = binarySearch(array, 0, len(array)-1, element)
+
 if result != -1:
-    print('Element found at index', result)
+	print("Element is present at index", result)
 else:
-    print('Element not in input array')
+	print("Element is not present in array")
